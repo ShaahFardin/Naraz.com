b3b698c0fa1c3f6820d8cbe2715441eb2d556468
shaahfardin <fardinshaah@gmail.com>
2022-12-09 00:10:34 +0600

    Product list page added with filtering and sorting option

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 
 function App() {
   return (
     <>
-     <Home/>
+     <ProductList/>
     </>
   );
 }
